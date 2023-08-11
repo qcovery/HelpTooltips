c6753bdf2646af63a8f8c32e22c8d59e442048ac
Oliver Stöhr <stoehr@effective-webwork.de>
2023-08-11 15:51:00 +0200

    Update

--- src/HelpTooltips/View/Helper/HelpTooltips/HelpTooltips.php
+++ src/HelpTooltips/View/Helper/HelpTooltips/HelpTooltips.php
@@ -37,9 +37,9 @@ class HelpTooltips extends \Laminas\View\Helper\AbstractHelper
     }
 
     public function showHelp () {
-        if ($_POST['showHelp']) {
+        if (isset($_POST['showHelp']) && $_POST['showHelp']) {
             $this->session->showHelp = true;
-        } else if ($_POST['hideHelp']) {
+        } else if (isset($_POST['hideHelp']) && $_POST['hideHelp']) {
             $this->session->showHelp = false;
         }
 
